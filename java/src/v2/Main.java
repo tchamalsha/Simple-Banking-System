bfe0e6248cf00b5602278cbcd53a914df446a078
tchamalsha <tharu.chamalsha@gmail.com>
2021-10-11 12:40:16 +0530

    add switch case

--- java/src/v2/Main.java
+++ java/src/v2/Main.java
@@ -21,9 +21,34 @@ public class Main {
         //initializing an account
         Account account=new Account(person.accountNumber, person.name);
 
-        //check account balance
-        AccountBalanceChecker accountBalanceChecker=new AccountBalanceChecker(account.getAccountBalance());
-        accountBalanceChecker.checkAccountBalance();
+        boolean action=true;
+        while(action){
+            System.out.println("*************************************************");
+            System.out.println("1.Check your balance\n" +
+                    "2.Make a deposit\n" +
+                    "3.Make a withdrawal\n" +
+                    "4.Calculate Interest\n" +
+                    "5.Print statement\n" +
+                    "6.Check previous transaction.\n" +
+                    "7.Exit the application");
+            System.out.println("Select from the menu: ");
+            int actionNumber=scanner.nextInt();
+            System.out.println("-------------------------------------------------");
+            switch (actionNumber){
+                case 1:
+                    //check account balance
+                    AccountBalanceChecker accountBalanceChecker=new AccountBalanceChecker(account.getAccountBalance());
+                    accountBalanceChecker.checkAccountBalance();
+                    break;
+                case 7:
+                    action=false;
+                    System.out.println("Thanks for banking with us. !!");
+                    break;
+            }
+        }
+
+
+
 
 
     }
